ea57e78a1e0e5789947d0cc5c0ba9c543bb49996
Xinbo <xinboliu555@gamil.com>
2026-04-09 13:49:42 +0200

    fusionado AuthResponse con RefreshResponse

--- app/src/main/java/com/xinlei/frontend/linkoria/app/auth/data/remote/dto/AuthResponse.kt
+++ app/src/main/java/com/xinlei/frontend/linkoria/app/auth/data/remote/dto/AuthResponse.kt
@@ -4,6 +4,6 @@ data class AuthResponse(
     val accessToken: String,
     val refreshToken: String,
     val tokenType: String,
-    val userId: String,
-    val username: String
+    val userId: String?,
+    val username: String?
 )
--- app/src/main/java/com/xinlei/frontend/linkoria/app/auth/data/remote/dto/RefreshResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.xinlei.frontend.linkoria.app.auth.data.remote.dto
-
-data class RefreshResponse(
-    val accessToken: String,
-    val refreshToken: String,
-    val tokenType: String
-)
