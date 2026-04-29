1a2186de0e988f3d1889db9857dd2efd2ea4f933
xinleio chen <xinleichen071130@gmail.com>
2026-04-29 21:12:21 +0200

    Codigo limpio con mejor la estructura

--- app/src/main/java/com/xinlei/frontend/linkoria/app/menu/ProfileFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.xinlei.frontend.linkoria.app.menu
-
-import androidx.fragment.app.Fragment
-import com.xinlei.frontend.linkoria.app.R
-
-class ProfileFragment : Fragment(R.layout.fragment_profile)
